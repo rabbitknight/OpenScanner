19c2988f13d5baeaabce27f2305226f702e4efe8
瓜皮 <mottchenz@gmail.com>
2022-02-24 23:37:28 +0800

    feat: zxingEngine DECODE 简单完成

--- core/src/main/java/net/rabbitknight/open/scanner/core/C.kt
+++ core/src/main/java/net/rabbitknight/open/scanner/core/C.kt
@@ -2,4 +2,43 @@ package net.rabbitknight.open.scanner.core
 
 object C {
     const val CODE_SUCCESS = 0
+    const val CODE_FAIL = -1
+
+    /**
+     *
+     * Android Y8 format.
+     *
+     *
+     * Y8 is a YUV planar format comprised of a WxH Y plane only, with each pixel
+     * being represented by 8 bits. It is equivalent to just the Y plane from [.YV12]
+     * format.
+     *
+     *
+     * This format assumes
+     *
+     *  * an even width
+     *  * an even height
+     *  * a horizontal stride multiple of 16 pixels
+     *
+     *
+     *
+     * <pre> size = stride * height </pre>
+     *
+     *
+     * For example, the [android.media.Image] object can provide data
+     * in this format from a [android.hardware.camera2.CameraDevice] (if
+     * supported) through a [android.media.ImageReader] object. The
+     * [Image#getPlanes()][android.media.Image.getPlanes] will return a
+     * single plane containing the pixel data. The pixel stride is always 1 in
+     * [android.media.Image.Plane.getPixelStride], and the
+     * [android.media.Image.Plane.getRowStride] describes the vertical
+     * neighboring pixel distance (in bytes) between adjacent rows.
+     *
+     * @see android.media.Image
+     *
+     * @see android.media.ImageReader
+     *
+     * @see android.hardware.camera2.CameraDevice
+     */
+    const val Y8 = 0x20203859
 }
--- engine-zxing/src/main/java/net/rabbitknight/open/scanner/engine/zxing/ZXingEngine.kt
+++ engine-zxing/src/main/java/net/rabbitknight/open/scanner/engine/zxing/ZXingEngine.kt
@@ -1,14 +1,22 @@
 package net.rabbitknight.open.scanner.engine.zxing
 
-import com.google.zxing.DecodeHintType
-import com.google.zxing.MultiFormatReader
+import android.graphics.ImageFormat
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import net.rabbitknight.open.scanner.core.C
+import net.rabbitknight.open.scanner.core.ScannerException
 import net.rabbitknight.open.scanner.core.engine.Engine
 import net.rabbitknight.open.scanner.core.format.BarcodeFormat
 import net.rabbitknight.open.scanner.core.format.BarcodeFormat.*
 import net.rabbitknight.open.scanner.core.image.ImageWrapper
+import net.rabbitknight.open.scanner.core.result.BarcodeResult
 import net.rabbitknight.open.scanner.core.result.ImageResult
+import net.rabbitknight.open.scanner.core.result.Rect
+import java.lang.Math.min
 
 class ZXingEngine : Engine {
+    // TODO: shared buffer
+    private var buffer = ByteArray(1024 * 1024 * 1)
     private val zxingCore = MultiFormatReader().also {
         this.setFormat(QR_CODE)
     }
@@ -24,7 +32,32 @@ class ZXingEngine : Engine {
     }
 
     override fun decode(image: ImageWrapper): ImageResult {
-        TODO("map")
+        val supports = listOf(C.Y8, ImageFormat.YV12, ImageFormat.YUV_420_888)
+        if (supports.all { image.format != it }) {
+            throw ScannerException("not support format ${image.format}")
+        }
+        val width = image.width
+        val height = image.height
+        val timestamp = image.timestamp
+        // resize buffer
+        buffer.let {
+            val wanted = image.width * image.height
+            if (wanted > buffer.size) buffer = ByteArray(wanted)
+        }
+        // to bytebuffer
+        buffer.let {
+            image.planes[0].toByteArray(it, width, height)
+        }
+        val bitmap = buffer.let {
+            val source = PlanarYUVLuminanceSource(it, width, height, 0, 0, width, height, false)
+            BinaryBitmap(HybridBinarizer(source))
+        }
+        val result: ImageResult = try {
+            zxingCore.decode(bitmap).toImageResult(timestamp)
+        } catch (e: Exception) {
+            ImageResult(C.CODE_SUCCESS, timestamp, emptyMap())
+        }
+        return result
     }
 
     private fun map(format: BarcodeFormat): com.google.zxing.BarcodeFormat? {
@@ -48,5 +81,48 @@ class ZXingEngine : Engine {
             UPC_EAN_EXTENSION -> com.google.zxing.BarcodeFormat.UPC_EAN_EXTENSION
             else -> null
         }
+    }
+
+    private fun map(format: com.google.zxing.BarcodeFormat): BarcodeFormat? {
+        return when (format) {
+            com.google.zxing.BarcodeFormat.AZTEC -> AZTEC
+            com.google.zxing.BarcodeFormat.CODABAR -> CODABAR
+            com.google.zxing.BarcodeFormat.CODE_39 -> CODE_39
+            com.google.zxing.BarcodeFormat.CODE_93 -> CODE_93
+            com.google.zxing.BarcodeFormat.CODE_128 -> CODE_128
+            com.google.zxing.BarcodeFormat.DATA_MATRIX -> DATA_MATRIX
+            com.google.zxing.BarcodeFormat.EAN_8 -> EAN_8
+            com.google.zxing.BarcodeFormat.EAN_13 -> EAN_13
+            com.google.zxing.BarcodeFormat.ITF -> ITF
+            com.google.zxing.BarcodeFormat.MAXICODE -> MAXICODE
+            com.google.zxing.BarcodeFormat.PDF_417 -> PDF_417
+            com.google.zxing.BarcodeFormat.QR_CODE -> QR_CODE
+            com.google.zxing.BarcodeFormat.RSS_14 -> RSS_14
+            com.google.zxing.BarcodeFormat.RSS_EXPANDED -> RSS_EXPANDED
+            com.google.zxing.BarcodeFormat.UPC_A -> UPC_A
+            com.google.zxing.BarcodeFormat.UPC_E -> UPC_E
+            com.google.zxing.BarcodeFormat.UPC_EAN_EXTENSION -> UPC_EAN_EXTENSION
+            else -> null
+        }
+    }
+
+    private fun ImageWrapper.PlaneWrapper.toByteArray(out: ByteArray, width: Int, height: Int) {
+        buffer.rewind()
+        val size = buffer.remaining()
+        var position = 0
+        for (row in 0 until height) {
+            buffer.get(out, position, width)
+            position += width
+            buffer.position(min(size, buffer.position() - width + rowStride))
+        }
+    }
+
+    private fun Result.toImageResult(timestamp: Long): ImageResult {
+        val format = map(this.barcodeFormat)!!
+        val barcodeResult = BarcodeResult(
+            format, Rect(0, 0, 0, 0), this.text, this.rawBytes
+        )
+        val result = mapOf(format to listOf(barcodeResult))
+        return ImageResult(C.CODE_SUCCESS, timestamp, result)
     }
 }
