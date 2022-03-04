75d68e342fe45260fa2aebedf5225555e23ab069
瓜皮 <mottchenz@gmail.com>
2022-03-05 00:20:36 +0800

    feat: 添加wechat_engine

---
+++ engine-wechat/src/main/cpp/wechat_engine/wechat_engine.cpp
@@ -0,0 +1,18 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+//
+// Tencent is pleased to support the open source community by making WeChat QRCode available.
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+#include "opencv2/wechat_qrcode.hpp"
+
+#include "opencv2/core.hpp"
+#include "opencv2/core/utils/filesystem.hpp"
+
+namespace cv {
+    namespace wechat_qrcode {
+        class Test {
+
+        };
+    }  // namespace wechat_qrcode
+}  // namespace cv
