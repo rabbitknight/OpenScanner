c0cfb27478b95c27e55431523a15ac69750e891a
瓜皮 <mottchenz@gmail.com>
2022-03-08 22:03:43 +0800

    feat: wechat_engine_jni decode

--- engine-wechat/src/main/cpp/wechat_engine/wechat_engine_jni.cpp
+++ engine-wechat/src/main/cpp/wechat_engine/wechat_engine_jni.cpp
@@ -8,6 +8,7 @@
 #include <jni.h>
 
 using namespace cv::wechat_qrcode;
+using namespace std;
 
 extern "C"
 JNIEXPORT jlong JNICALL
@@ -72,4 +73,45 @@ Java_net_rabbitknight_open_scanner_engine_wechat_WeChatQRCode_detect(JNIEnv *env
     }
     env->SetIntArrayRegion(result, 0, size, rects);
     return result;
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_net_rabbitknight_open_scanner_engine_wechat_WeChatQRCode_decode(JNIEnv *env, jobject thiz,
+                                                                     jlong peer, jbyteArray image,
+                                                                     jint width, jint height,
+                                                                     jintArray candidate_point) {
+    if (0 == peer) return nullptr;
+    auto *qrcode = (WeChatEngine *) peer;
+    auto *buffer = env->GetByteArrayElements(image, NULL);
+    // cvt bytebuffer to Mat
+    cv::Mat gray = cv::Mat(width, height, CV_8UC1, buffer);
+    // cvt candidate_points
+    auto *rect = env->GetIntArrayElements(candidate_point, NULL);
+    auto point = cv::Mat(4, 2, CV_32FC1);
+    int left = rect[0];
+    int top = rect[1];
+    int right = rect[2];
+    int bottom = rect[3];
+    point.at<float>(0, 0) = left;
+    point.at<float>(0, 1) = top;
+    point.at<float>(1, 0) = right;
+    point.at<float>(1, 1) = top;
+    point.at<float>(2, 0) = right;
+    point.at<float>(2, 1) = bottom;
+    point.at<float>(3, 0) = left;
+    point.at<float>(3, 1) = bottom;
+    env->ReleaseIntArrayElements(candidate_point, rect, 0);
+    auto points = vector<cv::Mat>();
+    points.push_back(point);
+    // decode
+    auto res_points = vector<cv::Mat>();
+    auto res_texts = qrcode->decode(gray, points, res_points);
+    // release bytebuffer
+    env->ReleaseByteArrayElements(image, buffer, 0);
+
+    // cvt result
+    if (res_points.empty() || res_texts.empty()) return nullptr;
+    auto text = res_texts[0];
+    return env->NewStringUTF(text.c_str());
 }
--- engine-wechat/src/main/java/net/rabbitknight/open/scanner/engine/wechat/WeChatQRCode.kt
+++ engine-wechat/src/main/java/net/rabbitknight/open/scanner/engine/wechat/WeChatQRCode.kt
@@ -25,7 +25,7 @@ class WeChatQRCode {
         get() = Companion.isAvailable
 
     /**
-     * qrcode识别
+     * qrcode检测 返回兴趣区域
      * @param image 图像 必须是Y8数据，与CV_8UC1 对应
      * @param width y 宽度
      * @param height y 高度
@@ -47,6 +47,45 @@ class WeChatQRCode {
     }
 
     /**
+     * qrcode 识别
+     * @param image 图像 必须是Y8数据，与CV_8UC1 对应
+     * @param width y 宽度
+     * @param height y 高度
+     * @param rois  region of interest,如果没有则进行一次检测
+     * @param outRects 二维码框
+     * @param outTexts 二维码值
+     * @return true代表正常检测(但是不一定有结果),false代表检测失败
+     */
+    fun decode(
+        image: ByteArray,
+        width: Int,
+        height: Int,
+        rois: List<Rect>,
+        outRects: MutableList<Rect>,
+        outTexts: MutableList<String>
+    ): Boolean {
+        outRects.clear()
+        outTexts.clear()
+        if (peer == 0L) return false
+        // if not roi,detect once
+        val rects = rois.ifEmpty {
+            val result = mutableListOf<Rect>()
+            detect(image, width, height, result)
+            result
+        }
+        // decode every roi to get barcode
+        rects.forEach {
+            val candidate_point = intArrayOf(it.left, it.top, it.right, it.bottom)
+            val text = decode(peer, image, width, height, candidate_point)
+            if (text != null) {
+                outRects.add(it)
+                outTexts.add(text)
+            }
+        }
+        return true
+    }
+
+    /**
      * 释放解码器资源
      * 一旦调用这个方法 将不能再被使用
      */
@@ -55,6 +94,10 @@ class WeChatQRCode {
         peer = 0
     }
 
+    // region Native Bridge
+    /**
+     * init wechat_engine with model/proto
+     */
     private external fun init(
         detector_prototxt_path: String,
         detector_caffe_model_path: String,
@@ -62,9 +105,29 @@ class WeChatQRCode {
         super_resolution_caffe_model_path: String
     ): Long
 
+    /**
+     * release wechat_engine
+     */
     private external fun release(peer: Long)
 
+    /**
+     * detect candidate_points for image
+     */
     private external fun detect(peer: Long, image: ByteArray, width: Int, height: Int): IntArray?
+
+    /**
+     * decode with single candidate_point
+     * combine all points on outside
+     */
+    private external fun decode(
+        peer: Long,
+        image: ByteArray,
+        width: Int,
+        height: Int,
+        candidate_point: IntArray
+    ): String?
+
+    // endregion Native Bridge
 
     init {
         val detector_prototxt_path = getFile(C.ASSET_DETECT_PROTO)?.absolutePath ?: ""
