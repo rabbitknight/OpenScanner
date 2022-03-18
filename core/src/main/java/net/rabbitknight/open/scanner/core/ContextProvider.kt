f9837ee96c5b4c71593f0be134d22fdb0381beac
rabbitknight <mottchenz@gmail.com>
2022-03-18 20:48:57 +0800

    feat: add ContextProvider

---
+++ core/src/main/java/net/rabbitknight/open/scanner/core/ContextProvider.kt
@@ -0,0 +1,47 @@
+package net.rabbitknight.open.scanner.core
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import androidx.core.content.ContentProviderCompat
+
+
+internal class ContextProvider : ContentProvider() {
+    companion object {
+
+        private lateinit var provider: ContentProvider
+
+        /**
+         * 获取全局上下文
+         */
+        fun context() = ContentProviderCompat
+            .requireContext(provider)
+    }
+
+    override fun onCreate(): Boolean {
+        provider = this
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? = null
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int = -1
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int = -1
+}
