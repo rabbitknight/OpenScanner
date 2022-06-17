0b54c6de1522cdf28f1cad1cc48af6cdfd824694
rabbitknight <mottchenz@gmail.com>
2022-06-17 23:53:47 +0800

    feat: Image Owner置于末尾

--- core/src/main/java/net/rabbitknight/open/scanner/core/image/_ImageWrapper.kt
+++ core/src/main/java/net/rabbitknight/open/scanner/core/image/_ImageWrapper.kt
@@ -5,11 +5,11 @@ import android.media.Image
 
 
 fun ByteArray.wrap(
-    owner: WrapperOwner<ByteArray>,
     @ImageFormat.Format format: String,
     width: Int,
     height: Int,
-    timestamp: Long
+    timestamp: Long,
+    owner: WrapperOwner<ByteArray>,
 ): ImageWrapper<ByteArray> =
     ByteArrayImage(owner, this, format, width, height, timestamp)
 
@@ -19,7 +19,7 @@ fun Image.wrap(
     Camera2Image(owner, this)
 
 fun Bitmap.wrap(
+    timestamp: Long,
     owner: WrapperOwner<Bitmap>,
-    timestamp: Long
 ): ImageWrapper<Bitmap> =
     BitmapImage(owner, this, timestamp)
