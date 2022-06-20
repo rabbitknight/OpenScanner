74c7b0b70b6cc18d4c0a327610182f107b0918cf
rabbitknight <mottchenz@gmail.com>
2022-06-21 00:12:43 +0800

    feat: copy CameraUtils代码
    https://developers.google.com/ml-kit/vision/barcode-scanning/android#kotlin

---
+++ core/src/main/java/net/rabbitknight/open/scanner/core/utils/CameraUtils.kt
@@ -0,0 +1,53 @@
+package net.rabbitknight.open.scanner.core.utils
+
+import android.app.Activity
+import android.content.Context.CAMERA_SERVICE
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.os.Build
+import android.util.SparseIntArray
+import android.view.Surface
+import androidx.annotation.RequiresApi
+
+object CameraUtils {
+    private val ORIENTATIONS = SparseIntArray()
+
+    init {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0)
+        ORIENTATIONS.append(Surface.ROTATION_90, 90)
+        ORIENTATIONS.append(Surface.ROTATION_180, 180)
+        ORIENTATIONS.append(Surface.ROTATION_270, 270)
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Throws(CameraAccessException::class)
+    private fun getRotationCompensation(
+        cameraId: String,
+        activity: Activity,
+        isFrontFacing: Boolean
+    ): Int {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        val deviceRotation = activity.windowManager.defaultDisplay.rotation
+        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+
+        // Get the device's sensor orientation.
+        val cameraManager = activity.getSystemService(CAMERA_SERVICE) as CameraManager
+        val sensorOrientation = cameraManager
+            .getCameraCharacteristics(cameraId)
+            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+
+        if (isFrontFacing) {
+            rotationCompensation = (sensorOrientation + rotationCompensation) % 360
+        } else { // back-facing
+            rotationCompensation = (sensorOrientation - rotationCompensation + 360) % 360
+        }
+        return rotationCompensation
+    }
+}
